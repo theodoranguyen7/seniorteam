092557216963105704a63f80c571d97772f043d8
Andrew Dao <Andrew@Andrews-MBP.attlocal.net>
2021-02-25 20:45:59 -0800

    trying

--- front/src/App.js
+++ front/src/App.js
@@ -3,18 +3,19 @@ import { Switch, Route } from 'react-router-dom';
 import React from 'react';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-        <Switch>
-          <Route exact path='/'>
-            <Home/>
-          </Route>
-          <Route exact path='/Login'>
-            <Login/>
-          </Route>
-        </Switch>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/Login'>
+          <Login />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { Navbar, Nav, Button, Form, Jumbotron } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Home () {
-    return (<h1>fitness</h1>);
+    return (<div> 
+        <Navbar bg="light" expand="lg">
+    <Navbar.Brand href="#home">Fittness App Demo</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+      </Nav>
+      <Form inline>
+        <Nav.Link href="/Login">Login</Nav.Link>
+        <Nav.Link href="#home">Sign Up</Nav.Link>
+      </Form>
+    </Navbar.Collapse>
+  </Navbar>
+
+  <Jumbotron>
+    <h1>Fitness Workout Log</h1>
+    <p>
+      Reach your personal goals, Get Connected with a trainer today.
+</p>
+    <p>
+      <Button variant="primary">Learn more</Button>
+    </p>
+  </Jumbotron>
+  </div>);
 }
 
 export default Home;
